5cb83fb739fb698c12e7c480e863d50a8af142f1
Feras Alsinan <saf_523_@hotmail.com>
2024-06-25 12:46:30 +0300

    maintainable login code

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import { Pressable, Text, View, StyleSheet, TextInput } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { Link } from 'expo-router';
-import AnimatedCoverImage from "@/components/login/AnimatedCoverImage"
+import { Pressable, Text, View, StyleSheet, ScrollView } from 'react-native';
+import AnimatedCoverImage from "@/components/login/AnimatedCoverImage";
+import Login from '@/components/login/login';
+import Signup from '@/components/login/signup';
 
-export default function index() {
+export default function Index() {
     const [activeButton, setActiveButton] = useState('LOGIN');
 
-    const handlePress = (button: any) => {
+    const handlePress = (button: string) => {
         setActiveButton(button);
     };
 
     return (
         <View style={styles.container}>
             <AnimatedCoverImage />
-            <View style={styles.containerLoginSignup}>
+            <ScrollView style={styles.containerLoginSignup} showsVerticalScrollIndicator={false}>
                 <View style={styles.loginSignup}>
                     <Pressable onPress={() => handlePress('LOGIN')} style={[styles.button, activeButton === 'LOGIN' && styles.activeButton]}>
                         <Text style={[styles.btnText, activeButton === 'LOGIN' && styles.activeBtnText]}>LOGIN</Text>
@@ -23,34 +23,8 @@ export default function index() {
                         <Text style={[styles.btnText, activeButton === 'SIGNUP' && styles.activeBtnText]}>SIGNUP</Text>
                     </Pressable>
                 </View>
-                <View style={styles.formContainer}>
-                    <View style={styles.inputWrapper}>
-                        <Ionicons name='mail' style={styles.icon} />
-                        <Text style={styles.label}>Email</Text>
-                    </View>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Example@Lazywait.com"
-                        placeholderTextColor="#888"
-                        keyboardType="email-address"
-                    />
-                    <View style={styles.inputWrapper}>
-                        <Ionicons name='key' style={styles.icon} />
-                        <Text style={styles.label}>Password</Text>
-                    </View>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="*******"
-                        placeholderTextColor="#888"
-                        secureTextEntry
-                    />
-                    <Link href={"/(tabs)"} style={styles.submitButton}>
-                        <Pressable >
-                            <Text style={styles.submitButtonText}>Submit</Text>
-                        </Pressable>
-                    </Link>
-                </View>
-            </View>
+                {activeButton === 'LOGIN' ? <Login /> : <Signup />}
+            </ScrollView>
         </View>
     );
 }
@@ -91,49 +65,6 @@ const styles = StyleSheet.create({
     },
     activeBtnText: {
         color: '#ffff',
-        fontWeight: 'bold',
-    },
-    formContainer: {
-        padding: 20,
-    },
-    inputWrapper: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginTop: 10,
-    },
-    icon: {
-        fontSize: 24,
-        color: '#ff6347',
-        marginRight: 10,
-    },
-    label: {
-        fontSize: 20,
-    },
-    input: {
-        height: 50,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        paddingHorizontal: 20,
-        fontSize: 18,
-        marginVertical: 10,
-        borderWidth: 1,
-        borderColor: '#ddd',
-        elevation: 2,
-    },
-    submitButton: {
-        backgroundColor: '#B24B3D',
-        borderRadius: 10,
-        padding: 15,
-        textAlign: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
-        elevation: 2,
-    },
-    submitButtonText: {
-        textAlign: 'center',
-        color: '#fff',
-        fontSize: 18,
         fontWeight: 'bold',
     },
 });
---
+++ components/login/signup.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, Pressable } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Link } from 'expo-router';
+
+export default function Signup() {
+    return (
+        <View style={styles.formContainer}>
+            <View style={styles.inputWrapper}>
+                <Ionicons name='mail' style={styles.icon} />
+                <Text style={styles.label}>Email</Text>
+            </View>
+            <TextInput
+                style={styles.input}
+                placeholder="Example@Lazywait.com"
+                placeholderTextColor="#888"
+                keyboardType="email-address"
+            />
+            <View style={styles.inputWrapper}>
+                <Ionicons name='key' style={styles.icon} />
+                <Text style={styles.label}>Password</Text>
+            </View>
+            <TextInput
+                style={styles.input}
+                placeholder="*******"
+                placeholderTextColor="#888"
+                secureTextEntry
+            />
+            <View style={styles.inputWrapper}>
+                <Ionicons name='key' style={styles.icon} />
+                <Text style={styles.label}>Confirm Password</Text>
+            </View>
+            <TextInput
+                style={styles.input}
+                placeholder="*******"
+                placeholderTextColor="#888"
+                secureTextEntry
+            />
+            <Link href={"/(tabs)"} style={styles.submitButton}>
+                <Pressable>
+                    <Text style={styles.submitButtonText}>Submit</Text>
+                </Pressable>
+            </Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    formContainer: {
+        padding: 20,
+    },
+    inputWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    icon: {
+        fontSize: 24,
+        color: '#ff6347',
+        marginRight: 10,
+    },
+    label: {
+        fontSize: 20,
+    },
+    input: {
+        height: 50,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        fontSize: 18,
+        marginVertical: 10,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        elevation: 2,
+    },
+    submitButton: {
+        backgroundColor: '#B24B3D',
+        borderRadius: 10,
+        padding: 15,
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+        elevation: 2,
+    },
+    submitButtonText: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
